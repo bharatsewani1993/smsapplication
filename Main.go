dd87cac7af4505ba6b75d8b6a7a4d1f2251e3223
BHARAT SEWANI <bharatsewani1993@gmail.com>
2018-06-18 23:57:52 +0530

    Load Login Page

--- Main.go
+++ Main.go
@@ -4,18 +4,73 @@ import(
   "html/template"
   "net/http"
   "log"
-  "os"
-  "text/template"
   "fmt"
+  "database/sql"
+  _"github.com/go-sql-driver/mysql"
 )
 
 func login(w http.ResponseWriter, r *http.Request){
-   t,_ := template.ParseFiles("login.html","main.html")
-   t.Execute(w,nil)
+   t,err := template.ParseFiles("login.html")
+   if err != nil{
+     fmt.Println("Error: ",err)
+   }
+   fmt.Println(t.Execute(w,nil))
+}
+
+func verifyuser(w http.ResponseWriter, r *http.Request){
+  r.ParseForm()
+  fmt.Println("Form Elements ",r.Form)
+  //form validation
+       var validationerror []string
+      //check for required fields
+      if len(r.Form["email"][0]) == 0 {
+        validationerror = append(validationerror,"Email Id is required")
+        fmt.Println("Username is required")  //print on console
+        fmt.Fprintf(w,"failed") //print on client machine
+        return
+      }
+      if len(r.Form["password"][0]) == 0{
+        validationerror = append(validationerror,"Password required")
+        fmt.Println("Password is required") //Print on console
+        fmt.Fprintf(w,"failed") //print on client machine
+        return
+      }
+
+      //validate username and password from database
+        validatecreds(r.Form["email"][0],r.Form["password"][0])
+}
+
+func validatecreds(email string, password string) bool {
+   db := dbconnection()
+   stmt, err := db.Prepare("Select * from login_table where username=? and password=?")
+   if err != nil {
+     fmt.Printf("Error \n",err)
+   }
+
+   result, err := stmt.Exec(email,password)
+   if err != nil{
+     fmt.Printf("Error \n", err)
+   }
+
+   effect,err := result.RowsAffected()
+   if err != nil {
+     fmt.Println("Error \n",err)
+   }
+   fmt.Println("Effect ", effect)
+   return true
+}
+
+func dbconnection() (*sql.DB){
+  db, err := sql.Open("mysql","root:P3NT3ST3R@/twilioapp")
+  if err != nil {
+    fmt.Printf("Database Error \n",err)
+  }
+  return db
 }
 
 func main(){
    http.HandleFunc("/",login)
+   http.HandleFunc("/verifyuser/",verifyuser)
    err := http.ListenAndServe(":9090",nil)
    if err != nil{
      log.Fatal("ListenandServe",err)
--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# SMS sending web application with help of Twilio API. 
-This is Short Message Sending Web application where I am using Twilio API to send and recieve SMS. 
+# SMS sending web application with help of Twilio API.
+This is Short Message Sending Web application where I am using Twilio API to send and recieve SMS.
 
 ## Current Features
-1. Just Initiated 
+1. Load Login page
+2. Validate Form Input
 
 ## To Do
-1. Setup Template
-2. Login/Logout
-3. Send SMS
-4. Recieve SMS
-5. Check Send SMS Log
-6. Check Recieved SMS Log
+1. Login/Logout
+2. Send SMS
+3. Recieve SMS
+4. Check Send SMS Log
+5. Check Recieved SMS Log
+6. Add CSRF and XSS Protection
